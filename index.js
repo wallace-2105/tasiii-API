58cd6f9cb06128ef766181033ce1b9a4ec71d582
wallace-2105 <wallace.wcs83@gmail.com>
2026-08-28 22:58:19 -0300

    ajuste final na estrutura do projeto

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('Chegou na rota raiz')
 })
 
-app.post('/chamados', function(req, res){
+app.post('/chamados', function (req, res) {
     res.send('Chegou na rota raiz')
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('O servidor está rodando na porta 3000')
 })
